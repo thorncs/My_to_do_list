ab6d6ccc2b2e985cf709f018818ce58a7f8d396d
cthorn <c.thorn@sbcglobal.net>
2015-11-16 21:12:23 -0600

    Front page html now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
 
 # When she hits enter, the page updates, and now the page lists
 # "1: Buy peacock feathers" as a to-do item in a to-do list
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
