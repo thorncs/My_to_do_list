1da20d3f6eebdeee8f1e9955e485a087b1fce1ff
Carl Thorn <c.thorn@sbcglobal.net>
2016-02-07 11:33:03 -0600

    Adjust new list view to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.utils.html import escape
 from lists.models import Item, List
 from django.core.urlresolvers import resolve
 from django.test import TestCase
@@ -56,7 +57,13 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
-        
+     def test_validation_errors_are_sent_back_to_home_page_template(self):
+         response = self.client.post('/lists/new', data={'item_text': ''})
+         self.assertEqual(response.status_code, 200)
+         self.assertTemplateUsed(response, 'home.html')
+         expected_error = escape("You can't have an empty list item")
+         
+         self.assertContains(response, expected_error)        
      def test_redirects_after_post(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 #from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from lists.models import Item, List
@@ -12,7 +13,13 @@ def view_list(request, list_id):
     return render(request, 'list.html', {'list': list_})
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item =  Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+
+        item.full_clean()
+    except ValidationError:
+        error = "You can't have an empty list item"
+        return render(request, 'home.html',{"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
