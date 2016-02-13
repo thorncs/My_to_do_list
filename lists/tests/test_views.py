2067708ed00147df19272a93b67b021f63468bd1
Carl Thorn <c.thorn@sbcglobal.net>
2016-02-13 07:13:17 -0600

    Refactor hare-coded URLS out of templates

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -72,6 +72,16 @@ class ListViewTest(TestCase):
             data={'item_text': 'A new item for an existing list'}
         )
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))       
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data = {'item_text' : ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
@@ -90,6 +100,9 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
                      
-
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(),0)
+        self.assertEqual(Item.objects.count(),0)
         
 
