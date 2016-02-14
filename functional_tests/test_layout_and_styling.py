d252d3b94716987a2d4a64a6a68b45bc7986aa96
Carl Thorn <c.thorn@sbcglobal.net>
2016-02-14 08:32:15 -0600

    rename all item input ids and names. still broken

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -5,12 +5,12 @@ class LayoutAndStyling(FunctionalTest):
         self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
         # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_text')
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
         # she starts a new list and sees the input is nicely
         # centered there too
         inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_text')
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
          #Satisfied, they both go back to sleep
          
